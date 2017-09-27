02e86d170e966a051c997e23306e1d6b7481d7b0
radenka <rada.sedlakova@gmail.com>
2017-09-27 11:19:42 +0200

    Objects finished. Little mistakes may occur.

--- sdf_process.py
+++ sdf_process.py
@@ -19,11 +19,12 @@ if args.verbose:
 
 def open_read_file(filename):
 
-    atom_counts = list()
-    bond_counts = list()
-    atoms_all = list()
-    coordinates_all = list()
-    bond_data = list()
+    atom_counts = list()   # contains counts of atoms (int) of all molecules in file
+    bond_counts = list()   # contains counts of bonds (int) of all molecules in file
+    atoms_all = list()     # contains lists of elements of all molecules in file (one list == one molecule)
+    coordinates_all = list()   # contains lists of atom x,y,z coordinates (one list == one molecule)
+    bond_data = list()     # contains lists of tuples describing valence of atoms (tuple: 1st at, 3nd at, valence)
+                           # (one list == one molecule)
 
     with open(filename) as file:
         while True:
@@ -36,24 +37,25 @@ def open_read_file(filename):
                 atom_count = int(info[0:3])
                 bond_count = int(info[3:6])
 
-                atom_counts.append(atom_count)  # atom_count: position '012' in line
-                bond_counts.append(bond_count)  # bond_count: position '345' in line
+                atom_counts.append(atom_count)
+                bond_counts.append(bond_count)
 
-                coordinates = list()
-                atoms = list()
+                atoms = list()  # filled with element symbols; describes one molecule
+                coordinates = list()  # filled with x,y,z tuples; coordinates of atoms in one molecule
                 for i in range(atom_count):
                     line = file.readline()                
-                    atoms.append(line[31:34].strip())
+                    atoms.append(line[31:34].strip())   # element
                     x, y, z = (float(line[l: r]) for l, r in [(3, 10), (13, 20), (23, 30)])
                     coordinates.append((x, y, z))
 
-                atoms_all.append(atoms)
-                coordinates_all.append(coordinates)
+                atoms_all.append(atoms)   # (molecule list added to list describing whole SDF file)
+                coordinates_all.append(coordinates)  # local lists added to global ones
 
-                molecule_data = list()
+                molecule_data = list()  # stores information about atoms in a molecule and their valence
                 for i in range(bond_count):
                     line = file.readline()
-                    first, second, bond = (int(line[l: r]) for l, r in [(0,3), (3,6), (6,9)])
+                    # describes valence of both bonded atoms (1 = single bond etc.)
+                    first, second, bond = (int(line[l: r]) for l, r in [(0, 3), (3, 6), (6, 9)])
                     molecule_data.append((first, second, bond))
                 bond_data.append(molecule_data)
 
@@ -65,61 +67,61 @@ def open_read_file(filename):
                 if '$$$$' in line:
                     break
 
-    allinone = dict()
+    allinone = dict()   # stores all obtained data in one dictionary
     allinone['atom_counts'] = atom_counts
     allinone['bond_counts'] = bond_counts
     allinone['atoms_all'] = atoms_all
     allinone['coordinates'] = coordinates_all
     allinone['bond_data'] = bond_data
-    
+
     return allinone
 
 
 def max_distance(coordinates):
-    for n, molecule in enumerate(coordinates, start=1):
+    for n, molecule in enumerate(coordinates, start=1):  # enumerate used for chatty output
         maxdist = 0
-        atom1 = 1
+        atom1 = 1   # initialization required by PEP8
         atom2 = 1
-        
+
         for i in range(len(molecule)):
             for j in range(i+1, len(molecule)):
-                distance = math.sqrt(sum((molecule[i][k] - molecule[j][k])**2 for k in range(3)))
+                distance = math.sqrt(sum((molecule[i][k] - molecule[j][k])**2 for k in range(3)))  # nested lists
                 
                 if distance > maxdist:
                     maxdist = distance
-                    atom1 = i+1
+                    atom1 = i+1     # 1 added up in order to be understandable for human beings
                     atom2 = j+1
                     
         if args.verbose:
             print('{}: Maxdist between atms {a1} {a2}; {dist:.4f}'.format(n, a1=atom1, a2=atom2, dist=maxdist))
 
 
-def maxbonds_counters(bond_tuples, atom_counts, atoms_all):
-    glob_pairs = list()
+def maxbonds_counters(bond_tuples, atom_counts, atoms_all):   # atoms_all == lists of elements
+    glob_pairs = list()   # contains lists of tuples (element, max_bond), (one list == one molecule)
 
-    for mol_bonds, count, atoms in zip(bond_tuples, atom_counts, atoms_all):
+    for mol_bonds, count, atoms in zip(bond_tuples, atom_counts, atoms_all):  # atoms == elements
         maxbonds = [0] * count
-        
-        # values = tuple: (position of 1st atom, position of 2nd atom, bond type)
+
         for first_atom, second_atom, bond in mol_bonds:
-            if bond > maxbonds[first_atom-1]:   # if bonds > maxbonds[first_atom-1]
-                maxbonds[first_atom-1] = bond     # maxbonds[first-1] = bonds
+            if bond > maxbonds[first_atom-1]:   # atom positions starts at 1, not at 0 - subtraction needed
+                maxbonds[first_atom-1] = bond
             if bond > maxbonds[second_atom-1]:
                 maxbonds[second_atom-1] = bond
 
-        pairs_tuples = list(zip(atoms, maxbonds))
-        glob_pairs.append(pairs_tuples)
+        molecule_maxbond_tuples = list(zip(atoms, maxbonds)) # list of tuples created for each molecule [('N', 1), ('0', 2)]
+        # contains information about elements and their maximum bonds
+        glob_pairs.append(molecule_maxbond_tuples)
 
     # COUNTERS
-    final_stat = Counter()   # mol_counter = Counter([])
-    # final_stat += mol_counter (to same u elem_Counter)
-    for n, molecule_pairs in enumerate(glob_pairs, start=1):
+    final_stat = Counter()
+
+    for n, molecule_maxbond_tuples in enumerate(glob_pairs, start=1):
         mol_counter = Counter()
-        
-        for i in molecule_pairs:
+
+        for i in molecule_maxbond_tuples:
             mol_counter[i] += 1
         for key in mol_counter:
-            final_stat[key] += mol_counter[key]        
+            final_stat[key] += mol_counter[key]   # mol_counter[key] represents value
         
         if args.verbose:
             print('{}: {}'.format(n, mol_counter))
@@ -131,17 +133,19 @@ def maxbonds_counters(bond_tuples, atom_counts, atoms_all):
 
 def molecular_mass(elements_lists):
     help_reader = {}
+
     with open('Periodic_Table_Of_Elements.csv') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',')
         
-        for row in reader:  # help_reader is created in order to reduce opening of the csv file
+        for row in reader:  # help_reader is created in order to reduce opening of the csv file, effective data storing
             help_reader[row['Symbol']] = float(row['Atomic Weight'])
 
     for n, molecule in enumerate(elements_lists, start=1):
         """
         example: molecule = ['N', 'O', 'O', 'O', 'H', 'H', 'H', 'H', 'H']
         Repetition of elements. The summarizing element_sum Counter() is created
-        in order to reduce looping over keys in help_reader. """ 
+        in order to reduce looping over keys in help_reader. 
+        """
         
         element_sum = Counter()
         for element in molecule:
@@ -150,20 +154,18 @@ def molecular_mass(elements_lists):
         molecular_mass = 0
             
         for element, count in element_sum.items():
-            molecular_mass += help_reader[element] * count
-            """for PT_element, atomic_weight in help_reader.items():
-                if element == PT_element:
-                    molecular_mass += atomic_weight * count"""
-                    
+            molecular_mass += help_reader[element] * count   # getting value through dictionary
         fin_molecular_mass = molecular_mass * 1.66053904e-27
-                        
+
         if args.verbose:
             print('{}: Weight = {:.3e} kg'.format(n, fin_molecular_mass))
-            
-stack = open_read_file(args.filename)
-max_distance(stack['coordinates'])
-molecular_mass(stack['atoms_all'])
-maxbonds_counters(stack['bond_data'], stack['atom_counts'], stack['atoms_all'])
+
+
+if __name__ == '__main__':
+    stack = open_read_file(args.filename)
+    max_distance(stack['coordinates'])
+    molecular_mass(stack['atoms_all'])
+    maxbonds_counters(stack['bond_data'], stack['atom_counts'], stack['atoms_all'])
 
 print("\n--- %s seconds ---" % (time.time() - start_time))
 
--- sdf_process_objects.py
+++ sdf_process_objects.py
@@ -25,39 +25,81 @@ class Atom:
 
 
 class Molecule:
-    def __init__(self, atom_count, bond_count, atom_list, bond_list):
+    def __init__(self, atom_count, bond_count, atoms, bonds):
         self.atom_count = atom_count
         self.bond_count = bond_count
-        self.atom_list = atom_list
-        self.bond_list = bond_list
+        self.atoms = atoms
+        self.bonds = bonds
 
     def __str__(self):
         return 'Molecule: atom & bond count:{}, {}, \natms:{}, bnds:{}'.format(self.atom_count, self.bond_count,
-                                                                               self.atom_list, self.bond_list)
+                                                                               self.atoms, self.bonds)
 
     def maxdistance(self):
-
         maxdist = 0
         atom1 = 1    # initialization required by PEP8
         atom2 = 1
-        # print(len(self.atom_list)) # output: always 24
-        for i in range(len(self.atom_list)):
-            # print(self.atom_list[i].coords[0]) # output: always -4.4647
-            for j in range(i+1, len(self.atom_list)):  # namedtuple makes it ugly
-                distance = math.sqrt(sum((self.atom_list[i].coords[k] - self.atom_list[j].coords[k])**2 for k in range(3)))
+
+        for m, atom_a in enumerate(self.atoms):
+            for n, atom_b in enumerate(self.atoms[1:], start=m+1):
+                distance = math.sqrt(sum((atom_a.coords[k] - atom_b.coords[k])**2 for k in range(3)))
 
                 if distance > maxdist:
                     maxdist = distance
-                    atom1 = i+1
-                    atom2 = j+1
+                    atom1 = m+1
+                    atom2 = n+1-m    # m subtraction because of start=m+1
 
         if args.verbose:
             print('Maxdist between atms {a1} {a2}; {dist:.4f}'.format(a1=atom1, a2=atom2, dist=maxdist))
 
+    def molecular_mass(self):
+        element_sum = Counter()
+
+        for atom in self.atoms:
+            """
+            example: elements in a molecule = ['N', 'O', 'O', 'O', 'H', 'H', 'H', 'H', 'H']
+            Repetition of elements. The summarizing element_sum Counter() is created
+            in order to reduce looping over keys in relative_atomic_masses. 
+            """
+            element_sum[atom.element] += 1
+
+        molecular_mass = 0
+
+        for element, count in element_sum.items():
+            molecular_mass += relative_atomic_masses[element] * count  # getting value through dictionary
+        fin_molecular_mass = molecular_mass * 1.66053904e-27
+
+        if args.verbose:
+            print('Weight = {:.3e} kg'.format(fin_molecular_mass))
+
+    def maxbonds_counters(self):
+        maxbonds = [0] * self.atom_count
+
+        for i in self.bonds:
+            if i.bond > maxbonds[i.first_atom-1]:
+                maxbonds[i.first_atom-1] = i.bond
+            if i.bond > maxbonds[i.second_atom-1]:
+                maxbonds[i.second_atom-1] = i.bond
+
+        # molecule_maxbonds == list of tuples created for each molecule [('N', 1), ('0', 2)]
+        # contains information about elements and their maximum bonds
+        molecule_maxbonds = list(zip([atom.element for atom in self.atoms], maxbonds))
+
+        # COUNTERS
+        molecule_statistics = Counter()
+        for i in molecule_maxbonds:
+            molecule_statistics[i] += 1
+
+        for key in molecule_statistics:
+            final_statistics[key] += molecule_statistics[key]
+
+        if args.verbose:
+            print(molecule_statistics)
+
 
 def open_read_file(filename):
     # Coordinates = namedtuple('Coordinates', ['x', 'y', 'z'])
-    Bonds = namedtuple('Bonds', ['first_at', 'second_at', 'valence'])
+    Bonds = namedtuple('Bonds', ['first_atom', 'second_atom', 'bond'])
     molecules = list()
 
     with open(filename) as file:
@@ -67,34 +109,25 @@ def open_read_file(filename):
                     file.readline()
                 info = file.readline()
 
-                Molecule.atom_count = int(info[0:3])
-                Molecule.bond_count = int(info[3:6])
-                # print(Molecule.atom_count) # OK
+                atom_count = int(info[0:3])
+                bond_count = int(info[3:6])
 
-                Molecule.atom_list = list()  # list of Atom objects included in a molecule
-                for i in range(Molecule.atom_count):
+                atoms = list()  # list of Atom objects included in a molecule
+                for i in range(atom_count):
                     line = file.readline()
-                    Atom.element = line[31:34].strip()
-                    # Atom.coords = Coordinates((float(line[l: r]) for l, r in [(3, 10), (13, 20), (23, 30)]))
-                    """x, y, z = (float(line[l: r]) for l, r in [(3, 10), (13, 20), (23, 30)])
-                    Atom.coords = Coordinates(x, y, z)"""
-                    x, y, z = (float(line[l: r]) for l, r in [(3, 10), (13, 20), (23, 30)])
-                    Atom.coords = (x, y, z)
-                    # print(Atom.element)  # OK
-                    # print(Atom.coords)   # OK
-                    Molecule.atom_list.append(Atom)
+                    element = line[31:34].strip()
+                    coordinates = tuple(float(line[l: r]) for l, r in [(3, 10), (13, 20), (23, 30)])
+                    atom = Atom(coordinates, element)
+                    atoms.append(atom)
 
-
-                Molecule.bond_list = list()  # stores information about atoms in a molecule and their valence
-                for i in range(Molecule.bond_count):
+                bonds = list()  # stores information about atoms in a molecule and their valence
+                for i in range(bond_count):
                     line = file.readline()
-                    # originally: first, second, bond = (int(line[l: r]) for l, r in [(0, 3), (3, 6), (6, 9)])
-                    first_at, second_at, valence = (int(line[l: r]) for l, r in [(0, 3), (3, 6), (6, 9)])
-                    Molecule.bond_list.append(Bonds(first_at, second_at, valence))  # visually more complicated than before
-                                                                                    # previous version - better?
-                # print(Molecule.bond_list) # OK
-                # print(Molecule.atom_list[0].element) # output: H H H
-                molecules.append(Molecule)
+                    first_at, second_at, bond = (int(line[l: r]) for l, r in [(0, 3), (3, 6), (6, 9)])
+                    bonds.append(Bonds(first_at, second_at, bond))
+
+                molecule = Molecule(atom_count, bond_count, atoms, bonds)
+                molecules.append(molecule)
 
             except ValueError:
                 break
@@ -106,23 +139,30 @@ def open_read_file(filename):
 
     return molecules
 
-if __name__ == '__main__':
-    stack = open_read_file(args.filename)
-    #for molecule in stack:
-        #print(molecule.atom_list[0].coords)
 
-"""help_reader = dict()
-with open('Periodic_Table_Of_Elements.csv') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=',')
-    for row in reader:
-        help_reader[row['Symbol']] = float(row['Atomic Weight'])"""
-
-"""help_reader = dict()
-def get_molecular_mass_using_objects(external_file):
-
-    with open(external_file) as csvfile:
+def get_atomic_masses():
+    with open('Periodic_Table_Of_Elements.csv') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',')
         for row in reader:
-            help_reader[row['Symbol']] = float(row['Atomic Weight'])
+            relative_atomic_masses[row['Symbol']] = float(row['Atomic Weight'])
 
-get_molecular_mass_using_objects('Periodic_Table_Of_Elements.csv')"""
+    # periodic table storing - other possibility:
+    # relative_atomic_masses as local in get_atomic_masses, return it
+
+if __name__ == '__main__':
+    final_statistics = Counter()
+    relative_atomic_masses = dict()
+
+    stack = open_read_file(args.filename)
+    get_atomic_masses()
+
+    for n, molecule in enumerate(stack, start=1):
+        if args.verbose:
+            print(str(n)+':')
+        molecule.maxdistance()
+        molecule.molecular_mass()
+        molecule.maxbonds_counters()
+
+    print("\n### FINAL STATISTICS OF ATOM TYPES: ###")
+    print_final = pprint.PrettyPrinter(indent=2)
+    print_final.pprint(final_statistics)
